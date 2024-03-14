04b4a1ca704d42994573e0964900d692fed46c0a
FedorLuchkin <f.luchkin@mail.ru>
2024-03-14 23:48:54 +0300

    lab3_var1_7_recovery

---
+++ src/lab3_var1_7/Fraction.java
@@ -0,0 +1,73 @@
+package lab3_var1_7;
+
+import static lab1_var2_7.Main.getGreatestCommonDivisor;
+
+
+public class Fraction {
+    private int m;
+    private int n;
+
+    public Fraction() {
+        this.setM(1);
+        this.setN(1);
+    }
+    public Fraction(int m, int n) {
+        this.setM(m);
+        this.setN(n);
+    }
+
+    public int getM() {
+        return this.m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    public int getN() {
+        return this.n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void print() {
+        System.out.printf("%d/%d ", m, n);
+    }
+
+    public static Fraction add(Fraction first, Fraction second) {
+        int denominator = first.getN() * second.getN();
+        int numerator = first.getM() * second.getN() + first.getN() * second.getM();
+        return signFix(numerator, denominator);
+    }
+
+    public static Fraction subtract(Fraction first, Fraction second) {
+        int denominator = first.getN() * second.getN();
+        int numerator = first.getM() * second.getN() - first.getN() * second.getM();
+        return signFix(numerator, denominator);
+    }
+
+    public static Fraction multiply(Fraction first, Fraction second) {
+        int numerator = first.getM() * second.getM();
+        int denominator = first.getN() * second.getN();
+        return signFix(numerator, denominator);
+    }
+
+    public static Fraction divide(Fraction first, Fraction second) {
+        int numerator = first.getM() * second.getN();
+        int denominator = first.getN() * second.getM();
+        return signFix(numerator, denominator);
+    }
+
+    private static Fraction signFix(int numerator, int denominator) {
+        int commonDivisor = getGreatestCommonDivisor(numerator, denominator);
+        numerator = numerator/ commonDivisor;
+        denominator = denominator / commonDivisor;
+        if (denominator < 0) {
+            denominator = denominator * -1;
+            numerator = numerator * -1;
+        }
+        return new Fraction(numerator, denominator);
+    }
+}
